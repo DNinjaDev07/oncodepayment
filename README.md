7859c4e959f908ce6822105c3a1f95eccba050d6
DanielOnCode <daniel.ogorigbosuah@gmail.com>
2024-04-23 02:18:27 -0400

    Initialize oncodepayment

---
+++ README.md
@@ -0,0 +1,7 @@
+# OncodePayment
+
+The oncode payment project will create APIs to perform CRUD operations on
+payment APIs.
+
+The final version will be Dockerised with all dependencies, along with REST Assured and Newman with postman collection
+to run CI/CD tests wit Jenkins.
---
+++ src/main/java/com/nozik/oncodepayment/OncodepaymentApplication.java
@@ -0,0 +1,13 @@
+package com.nozik.oncodepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OncodepaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OncodepaymentApplication.class, args);
+    }
+
+}
